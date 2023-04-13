5eec8b9b88943876e9a0ddbb6b8267d5a72f1c94
Xavi <xaviertralleroguillot@gmail.com>
2023-04-13 16:44:13 +0200

    1st commit

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 // Iteration 1: Names and Input
+/*
+
+ 1.1 Create a variable `hacker1` with the driver's name. <br>
+  1.2 Print `"The driver's name is XXXX"`.<br>
+  1.3 Create a variable `hacker2` with the navigator's name.<br>
+  1.4 Print `"The navigator's name is YYYY"`.*/
+
+let hacker1 ="XavierTrallero"
+console.log("The driver's name is "hacker1");
+let hacker2="Yabel";
+console.log("The navigator's name is",+ "hacker2");
+
 
 
 // Iteration 2: Conditionals
 
+/*
+- The driver has the longest name, it has XX characters. or
+- It seems that the navigator has the longest name, it has XX characters. or
+- Wow, you both have equally long names, XX characters!.
+*/
 
 // Iteration 3: Loops
+
+/*
+.1 Print the characters of the driver's name, separated by space, and in capital letters, i.e., "J O H N".
+
+3.2 Print all the characters of the navigator's name in reverse order, i.e., "nhoJ".
+
+3.3 Depending on the lexicographic order of the strings, print:
+
+The driver's name goes first.
+Yo, the navigator goes first, definitely.
+What?! You both have the same name?
+*/
